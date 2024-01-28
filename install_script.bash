f304e661a57b728ee7edf972c0484121b43177c0
Usernamealready-taken <adrian.vidalfenero@gmail.com>
2024-01-27 22:54:26 -0500

    finished bash file

--- install_script.bash
+++ install_script.bash
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
 git clone https://github.com/EzraH442/mchacks-11.git
 
 cd mchacks-11
 
 pip install -r requirements.txt
 
-import module1
-import module2
-import module3
-
-
+if [ $? -eq 0 ]; then
+    echo "Installation completed successfully."
+else
+    echo "Error: Pip installation failed."
+fi
