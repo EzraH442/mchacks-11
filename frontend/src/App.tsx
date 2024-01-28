cddc4317a4cb266db7b643b72f2cf6982f15ab0a
Olivier Lefevre <olivierlefevre2035@gmail.com>
2024-01-28 08:56:34 -0500

    added cat

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -170,6 +170,8 @@ function App() {
             </div>
           </div>
         </div>
+        {training &&
+                ( <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeWo2cmMzd2R6MmZhbWlrM2Uzc3NnMHB1MmN4M3lxcDAzbjN3MnBpdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/GeimqsH0TLDt4tScGw/giphy.gif" alt="bong cat" /> )}
         <Button
           className="mt-4 mb-4"
           variant="outline"
@@ -217,6 +219,9 @@ function App() {
                 ) : (
                   <span>No results yet</span>
                 )}
+
+                
+
               </div>
             </div>
             {/* <table class="box" style="display: flex; flex-direction: row; width:20vw" >
