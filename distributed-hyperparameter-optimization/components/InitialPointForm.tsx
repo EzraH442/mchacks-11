d985908ce135e1a34e4242358e53da3d6cea83ec
Ezra Huang <ezrahuang155@gmail.com>
2024-02-23 18:28:34 -0500

    state not updating bug fixes

--- distributed-hyperparameter-optimization/components/InitialPointForm.tsx
+++ distributed-hyperparameter-optimization/components/InitialPointForm.tsx
@@ -17,47 +17,73 @@ import {
 import { getSnapshot } from 'mobx-state-tree';
 import { IOptions, IQUniform, IUniform } from '@/models/StagingArea';
 
-export const formSchema = z.object({
-  layers: z.number({
-    required_error: 'Layers is required',
-  }),
-  neuronsPerLayer: z.array(
-    z.number({
-      required_error: 'Neurons per layer is required',
-    }),
-  ),
-  epsilonMin: z
-    .number({ required_error: 'Epsilon min is required' })
-    .min(0, 'Epsilon min must be between 0 and 1')
-    .max(1, 'Epsilon min must be between 0 and 1'),
-  epsilonMax: z
-    .number({
-      required_error: 'Epsilon max is required',
-    })
-    .min(0, 'Epsilon max must be between 0 and 1')
-    .max(1, 'Epsilon max must be between 0 and 1'),
-  epsilonStep: z.number({
-    required_error: 'Epsilon step is required',
-  }),
-  learningRateMin: z
-    .number({
-      required_error: 'Learning rate min is required',
-    })
-    .min(0, 'Learning rate must be between 0 and 1')
-    .max(1, 'Learning rate must be between 0 and 1'),
-  learningRateMax: z
-    .number()
-    .min(0)
-    .max(1, 'Learning rate must be between 0 and 1'),
-  learningRateStep: z.number({
-    required_error: 'Learning rate step is required',
-  }),
-});
-
 interface HypFormProps {
-  onSubmit: (data: z.infer<typeof formSchema>) => void;
+  onSubmit: (data: any) => void;
   disabled?: boolean;
 }
+
+const formatValues = (values: any) => {
+  if (values === undefined) {
+    return 'Select an option';
+  }
+  if (typeof values === 'boolean') {
+    return values ? 'true' : 'false';
+  }
+  if (typeof values === 'object') {
+    return JSON.stringify(values);
+  }
+  return values;
+};
+
+const OptionPoint: React.FC<{
+  hyperparameterName: string;
+  optionId: string;
+}> = observer(({ hyperparameterName, optionId }) => {
+  const { stagingArea } = useStore(null);
+  const ss = stagingArea.hyperparameters.get(hyperparameterName)
+    ?.searchSpace as IOptions;
+  return (
+    <FormItem>
+      <FormLabel>{hyperparameterName}</FormLabel>
+      <Select
+        value={ss.selectedValue ?? ''}
+        onValueChange={(v) => ss.setSelectedValue(v)}
+      >
+        <SelectTrigger>
+          {formatValues(ss.optionMap.get(ss.selectedValue)?.value)}
+        </SelectTrigger>
+        <SelectContent>
+          {Array.from(ss.optionMap.entries()).map(([k, v]) => {
+            return (
+              <SelectItem key={k} value={v.id}>
+                {formatValues(v.value)}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </Select>
+    </FormItem>
+  );
+});
+
+const NumberPoint: React.FC<{
+  hyperparameterName: string;
+}> = observer(({ hyperparameterName }) => {
+  const { stagingArea } = useStore(null);
+  const ss = stagingArea.hyperparameters.get(hyperparameterName)
+    ?.searchSpace as IQUniform;
+
+  return (
+    <FormItem>
+      <FormLabel>{hyperparameterName}</FormLabel>
+      <Input
+        type="number"
+        defaultValue={ss?.selectedValue}
+        onChange={(e) => ss?.setQUniformValue(e.target.valueAsNumber)}
+      />
+    </FormItem>
+  );
+});
 
 const InitialPointsForm: React.FC<HypFormProps> = observer(
   ({ onSubmit, disabled }) => {
@@ -76,62 +102,6 @@ const InitialPointsForm: React.FC<HypFormProps> = observer(
 
     const keys = Array.from(stagingArea.hyperparameters.keys());
 
-    const formatValues = (values: any) => {
-      if (values === undefined) {
-        return 'Select an option';
-      }
-      if (typeof values === 'boolean') {
-        return values ? 'true' : 'false';
-      }
-      if (typeof values === 'object') {
-        return JSON.stringify(values);
-      }
-      return values;
-    };
-
-    const renderOptionPoint = (parameterName: string) => {
-      const ss = stagingArea.hyperparameters.get(parameterName)
-        ?.searchSpace as IOptions;
-      return (
-        <FormItem>
-          <FormLabel>{parameterName}</FormLabel>
-          <Select
-            value={ss.selectedValue ?? ''}
-            onValueChange={(v) => ss.setSelectedValue(v)}
-          >
-            <SelectTrigger>
-              {formatValues(ss.optionMap.get(ss.selectedValue)?.value)}
-            </SelectTrigger>
-            <SelectContent>
-              {Array.from(ss.optionMap.entries()).map(([k, v]) => {
-                return (
-                  <SelectItem key={k} value={v.id}>
-                    {formatValues(v.value)}
-                  </SelectItem>
-                );
-              })}
-            </SelectContent>
-          </Select>
-        </FormItem>
-      );
-    };
-
-    const renderNumberPoint = (parameterName: string) => {
-      const ss = stagingArea.hyperparameters.get(parameterName)
-        ?.searchSpace as IUniform;
-
-      return (
-        <FormItem>
-          <FormLabel>{parameterName}</FormLabel>
-          <Input
-            type="number"
-            defaultValue={ss?.selectedValue}
-            onChange={(e) => ss?.setUniformValue(e.target.valueAsNumber)}
-          />
-        </FormItem>
-      );
-    };
-
     return (
       <div>
         <Form {...form}>
@@ -146,11 +116,16 @@ const InitialPointsForm: React.FC<HypFormProps> = observer(
 
                   switch (entry?.parameterType) {
                     case 'choice':
-                      return renderOptionPoint(name);
+                      return (
+                        <OptionPoint
+                          hyperparameterName={name}
+                          optionId={name}
+                        />
+                      );
                     case 'uniform':
-                      return renderNumberPoint(name);
+                      return <NumberPoint hyperparameterName={name} />;
                     case 'quniform':
-                      return renderNumberPoint(name);
+                      return <NumberPoint hyperparameterName={name} />;
                     default:
                       return <></>;
                   }
